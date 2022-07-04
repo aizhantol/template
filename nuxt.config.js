15d5d10ced961d1b74cf1edbc9cab6fd4221c55a
aizhantol <aizhan.tolebayeva@nu.edu.kz>
2022-07-04 15:13:24 +0600

    feat(name): ch

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'aizhanspussy',
+    title: 'frontend-template',
     htmlAttrs: {
       lang: 'en',
     },
